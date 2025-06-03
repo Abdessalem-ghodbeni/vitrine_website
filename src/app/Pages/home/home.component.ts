0263b28faf56c2c03d1c5f9c3ca91ffade591c9c
abdessalem <slouma4ghodbeny@gmail.com>
2025-06-03 23:59:42 +0100

    fondateurs

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,11 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import Swiper from 'swiper';
-import { SwiperOptions } from 'swiper/types';
+import { Autoplay, EffectFade, Pagination } from 'swiper/modules';
+import { AboutComponent } from '../about/about.component';
+
+// Configurez Swiper pour utiliser les modules nécessaires
+Swiper.use([Autoplay, EffectFade, Pagination]);
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -15,20 +19,33 @@ export class HomeComponent {
   }
 
   private initSwiper() {
-    const swiperOptions: SwiperOptions = {
+    new Swiper('.swiper-img-scale', {
+      // Modules nécessaires
+      modules: [Autoplay, EffectFade, Pagination],
+
+      // Configuration spécifique
       effect: 'fade',
+      fadeEffect: {
+        crossFade: true, // Important pour un fondu fluide
+      },
       speed: 1500,
       autoplay: {
-        delay: 3000,
+        delay: 1500,
         disableOnInteraction: false,
       },
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
       },
-      loop: true, // Ajout important pour le défilement continu
-    };
+      loop: true,
 
-    new Swiper('.swiper-img-scale', swiperOptions);
+      // Paramètres critiques pour Angular
+      observer: true, // Surveille les changements du DOM
+      observeParents: true, // Surveille les changements des parents
+      observeSlideChildren: true, // Surveille les changements des slides
+
+      // Initialisation
+      init: true,
+    });
   }
 }
