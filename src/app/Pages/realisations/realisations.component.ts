b8dd905cdbeb33463d538074f1fd3081ed7cdd15
abdessalem <slouma4ghodbeny@gmail.com>
2025-06-04 19:39:41 +0100

    saving

--- src/app/Pages/realisations/realisations.component.ts
+++ src/app/Pages/realisations/realisations.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-realisations',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './realisations.component.html',
   styleUrl: './realisations.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './Pages/home/home.component';
+import { GallerieComponent } from './Pages/gallerie/gallerie.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'gallerie', component: GallerieComponent },
 ];
